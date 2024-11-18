7e1e8bc24c132a946f41eac878f1130e276dfe8d
James Ji <57771801+Sannnji@users.noreply.github.com>
2024-11-18 13:36:37 -0500

    setup commit

---
+++ Debug/lcd1602.d
@@ -0,0 +1,12 @@
+# FIXED
+
+lcd1602.o: ../lcd1602.c \
+ C:/Users/james/Documents/GitHub/CSC202_workspace/default_project/clock.h \
+ C:/Users/james/Documents/GitHub/CSC202_workspace/default_project/lcd1602.h \
+ C:/Users/james/Documents/GitHub/CSC202_workspace/default_project/LaunchPad.h
+
+C:/Users/james/Documents/GitHub/CSC202_workspace/default_project/clock.h:
+
+C:/Users/james/Documents/GitHub/CSC202_workspace/default_project/lcd1602.h:
+
+C:/Users/james/Documents/GitHub/CSC202_workspace/default_project/LaunchPad.h:
